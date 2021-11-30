966ab2a5163aac4a98fc47c77dc97e0940a09a80
unknown <dinhthien943@gmail.com>
2021-11-30 19:41:47 +0700

    My message

---
+++ components/PushNotification.js
@@ -0,0 +1,158 @@
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import React, { usestart_date, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform } from 'react-native';
+import { doc, getDoc} from "firebase/firestore"; 
+
+class Schedule {
+  constructor (pill, start_date, end_date, hour, minute, frequency, times, quantity ) {
+      this.pill = pill;
+      this.start_date = start_date;
+      this.end_date = end_date;
+      this.hour = hour;
+      this.minute = minute;
+      this.frequency = frequency;
+      this.times = times;
+      this.quantity = quantity;
+  }
+  toString() {
+      return this.pill + ': ' + this.start_date + ' - ' + this.end_date;
+  }
+}
+
+// Firestore data converter
+const scheduleConverter = {
+  toFirestore: (schedule) => {
+      return {
+          pill: schedule.pill,
+          start_date: schedule.start_date,
+          end_date: schedule.end_date,
+          hour: schedule.hour,
+          minute: schedule.minute,
+          frequency: schedule.frequency,
+          times: schedule.times,
+          quantity: schedule.quantity
+          };
+  },
+  fromFirestore: (snapshot, options) => {
+      const data = snapshot.data(options);
+      return new Schedule(data.pill, data.start_date, data.end_date, data.hour, data.minute, data.frequency, data.times, data.quantity);
+  }
+};
+
+const ref_retrieve = doc(db, "schedule", "rw3pqHgqa7Xbl85sDyXU").withConverter(scheduleConverter);
+const docSnap = await getDoc(ref_retrieve);
+if (docSnap.exists()) {
+  // Convert to City object
+  const schedule = docSnap.data();
+  // Use a City instance method
+  console.log(schedule.toString());
+} else {
+  console.log("No such document!");
+}
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+export default function App() {
+  const [expoPushToken, setExpoPushToken] = usestart_date('');
+  const [notification, setNotification] = usestart_date(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}>
+      <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await confirmPushNotification();
+          await schedulePushNotification();
+        }}
+      />
+    </View>
+  );
+}
+
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "Reminder!",
+      body: 'Remember to take your medicine.',
+      data: { data: 'goes here' },
+    },
+    trigger: { 
+      hour: schedule.hour,
+      minute: schedule.minute,
+      repeats: schedule.quantity 
+    },
+  });
+}
+async function confirmPushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "Success!",
+      body: 'Successfully schedule your medicine.',
+      data: { data: 'goes here' },
+    },
+    trigger: { 
+      second : 2,
+    },
+  });
+}
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      pill: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
